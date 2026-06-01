e37b09005427ba7464ae10fd1e47c17957573b1d
Ashutosh Singh Patel <patelashu450@gmail.com>
2026-06-01 17:51:57 +0530

    update

--- Day-4/Find_nth_Fibonacci_term.cpp
+++ Day-4/Find_nth_Fibonacci_term.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int term ;
+    int n_term ;
     int first_term = 0, second_term = 1, next_term;
 
-    cout <<"Enter the no of term has to be print:";
-    cin >> term;
+     cout <<"Enter the no of term has to be print:" ;
+        cin >> n_term ;
+    
 
-    cout << "Fibonacci series: ";
-    for(int i=1; i<= term; i++){
-        cout << first_term;
-        cout << ",";
-        next_term = first_term + second_term;
-
-        first_term = second_term;
-        second_term = next_term;
+    if  (n_term<=0) {
+       cout << first_term;
     }
+    else if  (n_term==1) {
+        cout << second_term;
+    }
+    else{
+        for(int i=2; i<= n_term; i++){
+           next_term = first_term + second_term;
+           first_term = second_term;
+           second_term = next_term;
+    } 
+    cout << "The " << n_term << "th term of the Fibonacci series is: " << next_term;
+}
+       
     return 0;
 }
